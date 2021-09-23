9cd24212abed5c20dc749a75136676d27c380b8b
Tanushree-NR <tanushreenr01@gmail.com>
2021-09-23 08:52:33 +0530

    second

--- app/src/main/java/com/example/authenticationapp/Login.java
+++ app/src/main/java/com/example/authenticationapp/Login.java
@@ -3,6 +3,7 @@ package com.example.authenticationapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.widget.Button;
@@ -20,45 +21,38 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        user_name=findViewById(R.id.email);
-        pass_word=findViewById(R.id.password);
+        user_name = findViewById(R.id.email);
+        pass_word = findViewById(R.id.password);
         Button btn_login = findViewById(R.id.btn_login);
         Button btn_sign = findViewById(R.id.btn_signup);
-        mAuth=FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         btn_login.setOnClickListener(v -> {
-            String email= user_name.getText().toString().trim();
-            String password=pass_word.getText().toString().trim();
-            if(email.isEmpty())
-            {
+            String email = user_name.getText().toString().trim();
+            String password = pass_word.getText().toString().trim();
+            if (email.isEmpty()) {
                 user_name.setError("Email is empty");
                 user_name.requestFocus();
                 return;
             }
-            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
-            {
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 user_name.setError("Enter the valid email");
                 user_name.requestFocus();
                 return;
             }
-            if(password.isEmpty())
-            {
+            if (password.isEmpty()) {
                 pass_word.setError("Password is empty");
                 pass_word.requestFocus();
                 return;
             }
-            if(password.length()<6)
-            {
+            if (password.length() < 6) {
                 pass_word.setError("Length of password is more than 6");
                 pass_word.requestFocus();
                 return;
             }
-            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
-                if(task.isSuccessful())
-                {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
                     startActivity(new Intent(Login.this, MainActivity.class));
-                }
-                else
-                {
+                } else {
                     Toast.makeText(Login.this,
                             "Please Check Your login Credentials",
                             Toast.LENGTH_SHORT).show();
@@ -66,6 +60,23 @@ public class Login extends AppCompatActivity {
 
             });
         });
-        btn_sign.setOnClickListener(v -> startActivity(new Intent(Login.this,Register.class )));
+        btn_sign.setOnClickListener(v -> startActivity(new Intent(Login.this, Register.class)));
+
+        handleIntent(getIntent());
+    }
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+    // ATTENTION: This was auto-generated to handle app links.
+    private void handleIntent (Intent intent){
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+        if (Intent.ACTION_VIEW.equals(appLinkAction) && appLinkData != null) {
+            String path = toString();
+            Toast.makeText(this,"Path="+path,Toast.LENGTH_LONG).show();
+
+        }
     }
 }
